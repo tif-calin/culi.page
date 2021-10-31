0d26eac19ac82f1c360a8cac93bd5b7cce6533db
culi <culitif@tuta.io>
2021-10-31 13:55:12 -0700

    add options to color utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,11 @@
-export const rgb2hex = rgb => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`;
+const rgb2hex = (rgb, fromString = true) => fromString
+  ? `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+  : `#${rgb.map(n => n.toString(16).padStart(2, '0')).join('')}`
+;
 
-export const hex2rgb = hex => /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex).map(str => parseInt(str, 16)).slice(1);
+const hex2rgb = hex => /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex).map(str => parseInt(str, 16)).slice(1);
 
-export const rgb2hsl = rgb => {
+const rgb2hsl = (rgb, toIntegers = false) => {
   const rgbNorm = rgb.map(c => c /= 255);
   const max = Math.max(...rgbNorm);
   const min = Math.min(...rgbNorm);
@@ -16,10 +19,20 @@ export const rgb2hsl = rgb => {
     h = (h + 360) % 360; // make sure not negative
   } else h = s = 0;
 
-  return [h, s, l];
+  return toIntegers 
+    ? [h, s, l].map((c, i) => Math.round(c * (i ? 100 : 1)))
+    : [h, s, l]
+  ;
 };
 
-export const rgb2hslIntegers = rgb => {
+const rgb2hslIntegers = rgb => {
   const hsl = rgb2hsl(rgb);
   return [Math.round(hsl[0]), Math.round(hsl[1] * 100), Math.round(hsl[2] * 100)];
+};
+
+export {
+  rgb2hex,
+  hex2rgb,
+  rgb2hsl,
+  rgb2hslIntegers,
 };
